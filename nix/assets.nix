fbc2e8827da051ec96c741b0a6d9a31752afc18d
Dio Kyrie <DioKyrie@proton.me>
2025-10-25 00:17:19 +0300

    Update assets.nix

--- nix/assets.nix
+++ nix/assets.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   src = ../assets;
   pnpmDeps = pnpm.fetchDeps {
     inherit src version pname;
-    hash = builtins.readFile .nix/pnpmDepsHash.text;
+    hash = builtins.readFile ./pnpmDepsHash.text;
     fetcherVersion = 2;
   };
   nativeBuildInputs = [
